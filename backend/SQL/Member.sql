e2e93e41631fe8939db82c8b13beabd0fda334a3
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2023-02-14 15:30:20 +0900

    채팅 api

---
+++ backend/SQL/Member.sql
@@ -0,0 +1,9 @@
+create table member
+(
+    id varchar(20) NOT NULL,
+    star int,
+    epilogue varchar(100),
+    number int,
+
+    primary key (id)
+);
---
+++ backend/SQL/Ordering.sql
@@ -0,0 +1,9 @@
+create table ordering
+(
+    checking int AUTO_INCREMENT,
+    id int,
+    shipping varchar(30),
+    price int,
+
+    primary key (id,shipping)
+);
